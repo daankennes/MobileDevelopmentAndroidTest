68e1ee45a2ea702a0dcccbd65e139715d49fe8d4
daank <32707088+daankennes@users.noreply.github.com>
2018-09-27 11:20:57 +0200

    Terugknop eindelijk in orde

--- app/src/main/java/velo/daank/com/velolijst/Map.java
+++ app/src/main/java/velo/daank/com/velolijst/Map.java
@@ -1,17 +1,33 @@
 package velo.daank.com.velolijst;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Map extends AppCompatActivity {
 
@@ -27,30 +43,83 @@ public class Map extends AppCompatActivity {
         Log.d("station", station);
 
 
-        //handle permissions first, before map is created. not depicted here
-
         //load/initialize the osmdroid configuration, this can be done
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
-        //inflate and create the map
-        setContentView(R.layout.activity_main);
 
-        map = (MapView) findViewById(R.id.map);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                int x = 0;
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, x);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        setContentView(R.layout.activity_map);
+
+        map = (MapView) findViewById(R.id.mapview);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
         IMapController mapController = map.getController();
-        mapController.setZoom(9);
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
-        mapController.setCenter(startPoint);
+        mapController.setZoom(19);
 
+        // gegevens ophalen
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "velo").allowMainThreadQueries().build();
+        Station s = db.stationDao().findByName(station);
+
+        GeoPoint startPoint = new GeoPoint(Double.parseDouble(s.getLat()), Double.parseDouble(s.getLng()));
+
+        Marker startMarker = new Marker(map);
+        startMarker.setTitle(station);
+        startMarker.setPosition(startPoint);
+        mapController.setCenter(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+
+        Button button = (Button) findViewById(R.id.back_button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+
+        setResult(RESULT_OK, intent);
+        killActivity();
+    }
+
+    public void killActivity(){
+        //super.finish();
+        Log.d("fin","finish");
+        super.finish();
+    }
+
 }
